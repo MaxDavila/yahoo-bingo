5f659d2e9cfeb6b179697e56841541f54ce5b03a
maxdavila <adavila0711@gmail.com>
2013-12-11 20:06:05 -0800

    add registration info

--- bingo.js
+++ bingo.js
@@ -1,2 +1,8 @@
 var socketClient = require('socket.io-client');
 var client = socketClient.connect('ws://yahoobingo.herokuapp.com');
+
+var RegInfo = {
+  name: 'Max Davila',
+  email: 'adavila0711@gmail.com',
+  url: 'https://github.com/MaxDavila/yahoo-bingo'
+};
