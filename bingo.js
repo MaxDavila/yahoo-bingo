2b170eff44f1cfb544743da2c50208d24cc90d31
maxdavila <adavila0711@gmail.com>
2013-12-11 20:11:11 -0800

    add BingoCard object and set it to payload

--- bingo.js
+++ bingo.js
@@ -1,14 +1,23 @@
 var socketClient = require('socket.io-client');
 var client = socketClient.connect('ws://yahoobingo.herokuapp.com');
 
+var BingoCard;
+
 var RegInfo = {
   name: 'Max Davila',
   email: 'adavila0711@gmail.com',
   url: 'https://github.com/MaxDavila/yahoo-bingo'
 };
 
+
 client.emit('register', RegInfo);
 
 client.on('connect', function(){
   console.log("connected to Yahoo bingo server!");
 });
+
+client.on('card', function(payload){
+  BingoCard = payload;
+  console.log(BingoCard.slots);
+});
+
