59e4abff88c627de48126bd50aec440391a03e02
maxdavila <adavila0711@gmail.com>
2013-12-11 20:17:09 -0800

    add message for win, lose and disconnect from server

--- bingo.js
+++ bingo.js
@@ -16,8 +16,23 @@ client.on('connect', function(){
   console.log("connected to Yahoo bingo server!");
 });
 
+client.on('disconnect', function(){
+  console.log("disconnected from server");
+});
+
 client.on('card', function(payload){
-  BingoCard = payload;
+  BingoCard = payload.slots;
   console.log(BingoCard.slots);
 });
 
+client.on('number', function(number){
+  console.log(number);
+});
+
+client.on('win', function(message){
+  console.log(message);
+});
+
+client.on('lose', function(message){
+  console.log(message);
+});
